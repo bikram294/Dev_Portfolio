8203dda49dc455ccc4bfe02d15c99492a51ff944
Bikramdeep Singh <bikram.singh294@gmail.com>
2022-04-13 09:06:51 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <HeaderSocials/>
 
         <div className='me'>
-          <img src={ME} alt="me"/>
+          <img src={ME} style={{position: 'relative', top: '-12%'}} alt="me"/>
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
